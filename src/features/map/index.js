0c2c03597eee0a1471d65ef12a5bf58af1e05177
sedx876 <swatkins876@gmail.com>
2020-08-10 12:09:31 -0400

    movement.js

--- src/features/map/index.js
+++ src/features/map/index.js
@@ -4,9 +4,12 @@ function Map(props) {
   return (
     <div
       style={{
+        position: 'relative',
+        top: '0px',
+        left: '0px',
         width: '800px',
         height: '400px',
-        backgroundColor: 'black',
+        backgroundColor: 'green',
         border: '4px solid white',
         margin: '10px auto'
       }}
