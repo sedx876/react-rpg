57679ee2fe52d83afdc6ffdf896ab608cc707843
sedx876 <swatkins876@gmail.com>
2020-08-10 12:37:54 -0400

    player movement

--- src/features/player/movement.js
+++ src/features/player/movement.js
@@ -1,18 +1,47 @@
-import store from '../../config/store';
+import store from '../../config/store'
+import { SPRITE_SIZE } from '../../config/constants'
 
 export default function handleMovement(player) {
+
+  function getNewPosition(direction) {
+    const oldPos = store.getState().player.position
+    switch(direction) {
+      case 'WEST':
+        return [ oldPos[0]-SPRITE_SIZE, oldPos[1] ]
+      case 'EAST':
+        return [ oldPos[0]+SPRITE_SIZE, oldPos[1] ]
+      case 'NORTH':
+        return [ oldPos[0], oldPos[1]-SPRITE_SIZE ]
+      case 'SOUTH':
+        return [ oldPos[0], oldPos[1]+SPRITE_SIZE ]
+    }
+  }
+
+  function dispatchMove(direction) {
+    store.dispatch({
+      type: 'MOVE_PLAYER',
+      payload: {
+        position: getNewPosition(direction)
+      }
+    })
+  }
+
   function handleKeyDown(e) {
     e.preventDefault()
 
     switch(e.keyCode) {
       case 37:
-        return directionMove('WEST')
-        case 38:
-        return directionMove('NORTH')
-        case 39:
-        return directionMove('EAST')
-        case 40:
-        return directionMove('SOUTH')
+        return dispatchMove('WEST')
+
+      case 38:
+        return dispatchMove('NORTH')
+
+      case 39:
+        return dispatchMove('EAST')
+
+      case 40:
+        return dispatchMove('SOUTH')
+
       default:
         console.log(e.keyCode)
     }
@@ -21,5 +50,6 @@ export default function handleMovement(player) {
   window.addEventListener('keydown', (e) => {
     handleKeyDown(e)
   })
-  return player 
+
+  return player
 }
